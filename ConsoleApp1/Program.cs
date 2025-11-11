806c56069aec79e44e78c82aca2b9b6dde7e7e38
Gan-zz <2377487@fife.ac.uk>
2025-11-11 13:15:30 +0000

    added variables, calculations and print values to the user

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,4 +2,28 @@
 //Aodhan Sim
 //04/11/2025
 
+//variables for each roll and bonus
+int strenght = 0;
+int intelligence = 0;
+int strengthBonus = 0;
+int intelligenceBonus = 0;
+
+//welcome the user
 Console.WriteLine("Welcome to the Character Validator!");
+
+// get strenght from user and set strenght to input
+Console.WriteLine("Please enter the character's strenght:");
+strenght = Convert.ToInt32(Console.ReadLine());
+
+// get intelligence from user and set intelligence to input
+Console.WriteLine("Please enter the character's intelligence:");
+intelligence = Convert.ToInt32(Console.ReadLine());
+
+// set strenght bonus and print to user
+strengthBonus = (strenght / 10 - 2);
+Console.WriteLine("Strenght bonus: " + strengthBonus);
+
+// set intelligence bonus and print to user
+intelligenceBonus = (intelligence / 10 - 2);
+Console.WriteLine("Intelligence bonus: " + intelligenceBonus);
+
