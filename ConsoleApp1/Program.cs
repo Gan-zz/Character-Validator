bfd5fa8476a8b586fbe6c1fb72c89f79edda21ac
Gan-zz <2377487@fife.ac.uk>
2025-11-17 10:08:48 +0000

    changed some text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,20 +15,20 @@ int intelligenceBonus = 0;
 Console.WriteLine("Welcome to the Character Validator!");
 
 // get strenght from user and set strenght to input
-Console.WriteLine("Please enter the character's strenght from (1,20):");
+Console.WriteLine("Please enter the character's strenght:");
 strenght = Convert.ToInt32(Console.ReadLine());
 while (strenght > 20 || strenght < 1)
 {
-    Console.WriteLine("Roll is out of range, please enter again:");
+    Console.WriteLine("Strength score invalid. It must be between 1 and 20. Please try again:");
     strenght = Convert.ToInt32(Console.ReadLine());
 }
 
 // get intelligence from user and set intelligence to input
-Console.WriteLine("Please enter the character's intelligence (1,20):");
+Console.WriteLine("Please enter the character's intelligence:");
 intelligence = Convert.ToInt32(Console.ReadLine());
 while (intelligence > 20 || intelligence < 1)
 {
-    Console.WriteLine("Roll is out of range, please enter again:");
+    Console.WriteLine("Intelligence score invalid. It must be between 1 and 20. Please try again:");
     intelligence = Convert.ToInt32(Console.ReadLine());
 }
 
