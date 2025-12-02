b883dcea9539cd991ec09cdb4ef3f5226e8491fd
Gan-zz <2377487@fife.ac.uk>
2025-12-02 12:32:29 +0000

    made new variables and change input validation

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,38 +3,42 @@
 //04/11/2025
 
 //variables for each roll and bonus
-int strenght = 0;
+int strength = 0;
 int intelligence = 0;
-
-
 int strengthBonus = 0;
 int intelligenceBonus = 0;
+bool intelligenceValid = false;
+bool strengthValid = false;
 
 //welcome the user
-
 Console.WriteLine("Welcome to the Character Validator!");
 
-// get strenght from user and set strenght to input
-Console.WriteLine("Please enter the character's strenght:");
-strenght = Convert.ToInt32(Console.ReadLine());
-while (strenght > 20 || strenght < 1)
+// get strenght from user and set strength to input
+Console.WriteLine("Please enter the character's strength:");
+strength = Convert.ToInt32(Console.ReadLine());
+strengthValid = strength >= 1 && strength <= 20;
+while (strengthValid == false)
 {
     Console.WriteLine("Strength score invalid. It must be between 1 and 20. Please try again:");
-    strenght = Convert.ToInt32(Console.ReadLine());
+    strength = Convert.ToInt32(Console.ReadLine());
+    strengthValid = strength >= 1 && strength <= 20;
 }
 
 // get intelligence from user and set intelligence to input
 Console.WriteLine("Please enter the character's intelligence:");
 intelligence = Convert.ToInt32(Console.ReadLine());
-while (intelligence > 20 || intelligence < 1)
+intelligenceValid = intelligence >= 1 && intelligence <= 20;
+
+while (intelligenceValid == false)
 {
     Console.WriteLine("Intelligence score invalid. It must be between 1 and 20. Please try again:");
     intelligence = Convert.ToInt32(Console.ReadLine());
+    intelligenceValid = intelligence >= 1 && intelligence <= 20;
 }
 
 // set strenght bonus and print to user
-strengthBonus = (strenght - 10) / 2;
-Console.WriteLine("Strenght bonus: " + strengthBonus);
+strengthBonus = (strength - 10) / 2;
+Console.WriteLine("Strength bonus: " + strengthBonus);
 
 // set intelligence bonus and print to user
 intelligenceBonus = (intelligence - 10) / 2;
